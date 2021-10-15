4a5e716aea52d430c789cd4ca2db82293adba607
GustasZ <zgustas@gmail.com>
2021-10-15 14:01:00 +0300

    Confirm Order Functionality

---
+++ Data/Migrations/20211014103955_Orders.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ElectronicsStore.Data.Migrations
+{
+    public partial class Orders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "CartItem",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShoppingUserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Total = table.Column<int>(type: "int", nullable: false),
+                    OrderStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Order_AspNetUsers_ShoppingUserId",
+                        column: x => x.ShoppingUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItem_OrderId",
+                table: "CartItem",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_ShoppingUserId",
+                table: "Order",
+                column: "ShoppingUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItem_Order_OrderId",
+                table: "CartItem",
+                column: "OrderId",
+                principalTable: "Order",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItem_Order_OrderId",
+                table: "CartItem");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItem_OrderId",
+                table: "CartItem");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "CartItem");
+        }
+    }
+}

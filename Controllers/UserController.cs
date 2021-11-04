d2189051a2e74f9223c5311cd7bb52fcac101289
GustasZ <zgustas@gmail.com>
2021-11-04 19:32:25 +0200

    fixed website wide css, started working on categories design

---
+++ Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using ElectronicsStore.Data;
+using ElectronicsStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ElectronicsStore.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ShoppingUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context,UserManager<ShoppingUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> MyOrders()
+        {
+            var Orders = _context.Order.Include(o => o.ShoppingUser).Where(order => order.ShoppingUserId == User.FindFirstValue(ClaimTypes.NameIdentifier));
+            return View(await Orders.ToListAsync());
+        }
+    }
+}
